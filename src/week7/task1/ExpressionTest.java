2cb1a25e773240ca3232a1b6b08c0fda815f3c62
Lê Thị Thùy Linh <43266146+17020853@users.noreply.github.com>
2018-11-03 00:34:54 +0700

    Create ExpressionTest.java

---
+++ src/week7/task1/ExpressionTest.java
@@ -0,0 +1,32 @@
+package week7.task1;
+
+public class ExpressionTest {
+    public static void main(String[] args) {
+        Addition addition = new Addition();
+        Subtraction subtraction = new Subtraction();
+        Multiplication multiplication = new Multiplication();
+        Division division = new Division();
+
+        Numeral num1 = new Numeral(1);
+        Numeral num2 = new Numeral(2);
+        Numeral num3 = new Numeral(3);
+        Numeral num10 = new Numeral(10);
+        Numeral num0 = new Numeral(0);
+
+        Numeral result = new Numeral((10 * 10 - 1 + 2 * 3) *(10 * 10 - 1 + 2 * 3));
+        System.out.println("expression: "+result.toString());
+
+        System.out.println("addition "+addition.Addition(num0,num2).toString());
+        System.out.println("subtraction "+subtraction.Subtraction(num1,num2).toString());
+        System.out.println("multiplication "+multiplication.Multiplication(num2,num3).toString());
+        System.out.println("division "+ division.Division(num2,num1).toString());
+        System.out.println("---------------------");
+        try {
+            System.out.println(division.Division(num10, num0).toString());
+        } catch (ArithmeticException e) {
+            System.out.println("ArithmeticException");
+        }
+
+
+    }
+}
