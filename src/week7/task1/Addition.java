2145fb83c669b0c43a37f35f611c409805281378
Lê Thị Thùy Linh <43266146+17020853@users.noreply.github.com>
2018-11-03 00:30:00 +0700

    Update Addition.java

--- src/week7/task1/Addition.java
+++ src/week7/task1/Addition.java
@@ -1,4 +1,18 @@
 package week7.task1;
 
-public class Addition {
-}
+public class Addition extends BinaryExpression {
+    private Expression left, right;
+
+    public Expression Addition(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+        Numeral numeral = new Numeral(left.evaluate() + right.evaluate());
+        Expression expression = numeral;
+        return expression;
+    }
+
+
+    public String toString() {
+        return " ";
+    }
+   }
