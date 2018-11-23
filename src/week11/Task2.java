cecb9656ce5789d839f4c088f691d739eb95ac19
Lê Thị Thùy Linh <43266146+17020853@users.noreply.github.com>
2018-11-23 16:31:41 +0700

    Update Task2.java

--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,35 @@
+
 package week11;
 
-public class Task2 {
+
+import java.util.ArrayList;
+
+
+public class task2 {
+    public static <T extends Comparable> void max(ArrayList<T> a) {
+        int n= a.size();
+        T max=a.get(0);
+        for (int i=1; i<n; i++){
+            if ((a.get(i)).compareTo(max) > 0) {
+                max = a.get(i);
+            }
+            
+        }
+        System.out.println("max =" +max);
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(58);
+        list1.add(-1);
+        list1.add(100);
+        max(list1);
+        
+        ArrayList<Character> list2 = new ArrayList<>();
+        //list2.add('f');
+        list2.add('a');
+        list2.add('A');
+        max(list2);
+        
+    }
+    
 }
